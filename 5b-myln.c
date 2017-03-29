3515106cab508d066e8e3168da94ef27199bd876
Nisha <nishaj1296@gmail.com>
2017-03-29 20:07:09 +0530

    UNIX LAB

---
+++ 5b-myln.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+int main(int argc,char **argv)
+{
+  if(argc<3||argc>4||(argc==4&&strcmp(argv[1],"-s")))
+  {
+    printf("Usage: ./a.out [-s] <org_file> <new_link>\n");
+    return 1;
+  }
+  if(argc==4)
+  {
+    if(symlink(argv[2],argv[3])==-1)
+    {
+      perror("Cannot create symbolic link\n");
+      return 1;
+    }  
+    else
+      printf("Symbolic link created\n");
+  }
+  else
+  {
+    if(link(argv[1],argv[2])==-1)
+    {
+      perror("Cannot create hard link\n");
+      return 2;
+    }  
+    else
+      printf("Hard link created\n");
+  }
+  return 0;
+}                
